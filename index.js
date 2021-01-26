050d526d5a67f94cc1fa4677790d08f18cb8b661
Woohyun Park <a-eve@naver.com>
2021-01-26 14:40:13 +0900

    Feat: Add moveTop and moveBottom

--- index.js
+++ index.js
@@ -5,18 +5,14 @@ function moveRight(self){
   self.parentNode.style.left = "-100%";
   self.parentNode.nextElementSibling.style.left = "0%";
   self.style.visibility = "hidden";
-  // self.style.opacity = "0%";
   if(self.parentNode.getElementsByClassName("main__left").length != 0){
     self.parentNode.getElementsByClassName("main__left")[0].style.visibility = "hidden";
-    // self.parentNode.getElementsByClassName("main__left")[0].style.opacity = "0%";
   }
   if(self.parentNode.nextElementSibling.getElementsByClassName("main__left").length != 0){
     self.parentNode.nextElementSibling.getElementsByClassName("main__left")[0].style.visibility = "visible";
-    // self.parentNode.nextElementSibling.getElementsByClassName("main__left")[0].style.opacity = "100%";
   }
   if(self.parentNode.nextElementSibling.getElementsByClassName("main__right").length != 0){
     self.parentNode.nextElementSibling.getElementsByClassName("main__right")[0].style.visibility = "visible";
-    // self.parentNode.nextElementSibling.getElementsByClassName("main__right")[0].style.opacity = "100%";
   }
 }
 
@@ -24,17 +20,26 @@ function moveLeft(self){
   self.parentNode.style.left = "100%";
   self.parentNode.previousElementSibling.style.left = "0%";
   self.style.visibility = "hidden";
-  // self.style.opacity = "0%";
   if(self.parentNode.getElementsByClassName("main__right").length != 0){
     self.parentNode.getElementsByClassName("main__right")[0].style.visibility = "hidden";
-    // self.parentNode.getElementsByClassName("main__right")[0].style.opacity = "0%";
   }
   if(self.parentNode.previousElementSibling.getElementsByClassName("main__left").length != 0){
     self.parentNode.previousElementSibling.getElementsByClassName("main__left")[0].style.visibility = "visible";
-    // self.parentNode.previousElementSibling.getElementsByClassName("main__left")[0].style.opacity = "100%";
   }
   if(self.parentNode.previousElementSibling.getElementsByClassName("main__right").length != 0){
     self.parentNode.previousElementSibling.getElementsByClassName("main__right")[0].style.visibility = "visible";
-    // self.parentNode.previousElementSibling.getElementsByClassName("main__right")[0].style.opacity = "100%";
   }
+}
+
+function moveTop(self, i){
+  self.parentNode.style.transform = "translateY(-93%)";
+  document.getElementsByClassName("nav__title")[0].style.left = "-50%";
+  document.getElementsByClassName("nav__title")[i].style.left = 0;
+}
+
+function moveBottom(self, i){
+  self.style.left = "-50%";
+  document.getElementsByClassName("nav__title")[0].style.left = 0;
+  console.log(document.getElementsByClassName("main__title")[i]);
+  document.getElementsByClassName("main__page")[i].style.transform = "translateY(0%)";
 }
