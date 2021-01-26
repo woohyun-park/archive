01112936dc3aa48c585e4c28e2dff6a8c50e3dbb
Woohyun Park <a-eve@naver.com>
2021-01-26 10:41:56 +0900

    Branch: Branch swipeHorizontal

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+function moveRight(self){
+  self.style.left = "-100%";
+  self.nextElementSibling.style.left = "0%";
+}
+
+function moveLeft(self){
+  self.style.left = "100%";
+  self.previousElementSibling.style.left = "0%";
+}
