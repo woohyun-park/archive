2298ad9a8411dd8cd7f98092960f4a2afb20deab
Woohyun Park <a-eve@naver.com>
2021-01-26 15:23:04 +0900

    Add: Add create button

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
+let themeColor = ["#587bbe", "#e3605e", "#42997b"];
+
 function moveRight(self){
   self.parentNode.style.left = "-100%";
   self.parentNode.nextElementSibling.style.left = "0%";
@@ -34,12 +36,17 @@ function moveLeft(self){
 function moveTop(self, i){
   self.parentNode.style.transform = "translateY(-93%)";
   document.getElementsByClassName("nav__title")[0].style.left = "-50%";
+  document.getElementsByClassName("main__crud")[0].style.visibility = "visible";
+  document.getElementsByClassName("main__crud")[0].style.color = themeColor[i-1];
+  document.getElementsByClassName("main__crud")[0].style.opacity = 1;
   document.getElementsByClassName("nav__title")[i].style.left = 0;
 }
 
 function moveBottom(self, i){
   self.style.left = "-50%";
   document.getElementsByClassName("nav__title")[0].style.left = 0;
+  document.getElementsByClassName("main__crud")[0].style.visibility = "hidden";
+  document.getElementsByClassName("main__crud")[0].style.opacity = 0;
   console.log(document.getElementsByClassName("main__title")[i]);
   document.getElementsByClassName("main__page")[i].style.transform = "translateY(0%)";
 }
