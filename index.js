02d11a4d05a7791da49a89d4dd1b6affbfb01545
Woohyun Park <woohyunpark.mail@gmail.com>
2021-01-31 13:43:37 +0900

    Feat: Display images properly

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 let themeColor = ["#587bbe", "#e3605e", "#42997b"];
 
+let embedTemp = null;
+
 function moveRight(right){
   let currentPage = right.parentNode;
   let nextPage = currentPage.nextElementSibling;
@@ -48,12 +50,25 @@ function moveLeft(left){
 }
 
 function moveTop(self, i){
+  let scroll = document.getElementsByTagName("body")[0];
   let currentPage = self.parentNode;
+  let currentContent = document.getElementsByClassName("main__content")[i-1];
+  let currentType = currentContent.getElementsByClassName("main__type");
   let currentCrud = document.getElementsByClassName("main__crud")[0];
   let navTitle = document.getElementsByClassName("nav__title");
   let navMenu = document.getElementsByClassName("nav__menu")[0];
 
+  setTimeout(function(){
+    scroll.style.overflowY = "auto";
+  }, 500);
   currentPage.style.transform = "translateY(-93%)";
+  currentContent.style.display = "flex";
+  setTimeout(function(){
+    currentContent.style.marginTop = "10vh";
+  }, 50);
+  for(let j = 0; j < currentType.length; j++){
+    currentType[j].style.backgroundColor = themeColor[i-1];
+  }
   currentCrud.style.visibility = "visible";
   currentCrud.style.color = themeColor[i-1];
   currentCrud.style.opacity = 1;
@@ -65,12 +80,19 @@ function moveTop(self, i){
 }
 
 function moveBottom(i){
+  let scroll = document.getElementsByTagName("body")[0];
   let currentPage = document.getElementsByClassName("main__page")[i];
+  let currentContent = document.getElementsByClassName("main__content")[i];
   let currentCrud = document.getElementsByClassName("main__crud")[0];
   let navTitle = document.getElementsByClassName("nav__title");
   let navMenu = document.getElementsByClassName("nav__menu")[0];
 
+  scroll.style.overflowY = "hidden";
   currentPage.style.transform = "translateY(0%)";
+  currentContent.style.marginTop = "110vh";
+  setTimeout(function(){
+    currentContent.style.display = "none";
+  }, 500);
   currentCrud.style.visibility = "hidden";
   currentCrud.style.opacity = 0;
   navTitle[0].style.left = 0;
@@ -78,4 +100,65 @@ function moveBottom(i){
   navMenu.style.visibility = "hidden";
   navMenu.style.opacity = 0;
   navMenu.style.right = "-50%";
+}
+
+function showContent(self){
+  let main = document.getElementsByTagName("main")[0];
+  let siblings = getSiblings(self);
+  let image = self.getElementsByTagName("img")[0];
+  let type = self.getElementsByClassName("main__type")[0];
+  let text = self.getElementsByTagName("object")[0];
+
+  for(let i = 0; i < siblings.length; i++){
+    siblings[i].style.display = "none";
+  }
+
+  main.style.overflow = "auto";
+  self.style.maxWidth = "100vw";
+  self.style.maxHeight = "50vw";
+  self.style.width = "100vw";
+  self.style.height = "50vw";
+  image.style.maxWidth = "500px";
+  image.style.maxHeight = "500px";
+  image.style.width = "50vw";
+  image.style.height = "50vw";
+  type.style.visibility = "hidden";
+  text.style.display = "block";
+  self.firstElementChild.onclick = function() {hideContent(this.parentNode);};
+}
+
+function hideContent(self){
+  let main = document.getElementsByTagName("main")[0];
+  let siblings = getSiblings(self);
+  let image = self.getElementsByTagName("img")[0];
+  let type = self.getElementsByClassName("main__type")[0];
+  let text = self.getElementsByTagName("object")[0];
+
+  for(let i = 0; i < siblings.length; i++){
+    siblings[i].style.display = "block";
+  }
+
+  main.style.overflow = "hidden";
+  self.style.maxWidth = "340px";
+  self.style.maxHeight = "340px";
+  self.style.width = "26vw";
+  self.style.height = "26vw";
+  image.style.maxWidth = "340px";
+  image.style.maxHeight = "340px";
+  image.style.width = "26vw";
+  image.style.height = "26vw";
+  type.style.visibility = "visible";
+  text.style.display = "none";
+  self.firstElementChild.onclick = function() {showContent(this.parentNode);};
+}
+
+function getSiblings(self){
+  let siblings = [];
+  let childs = self.parentNode.children;
+  for(let i = 0; i < childs.length; i++){
+    if(childs[i] != self){
+      siblings.push(childs[i]);
+    }
+  }
+  return siblings;
 }
