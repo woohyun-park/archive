2247617c340c984ca872117ba75906d01fb03e80
Woohyun Park <woohyunpark.mail@gmail.com>
2023-01-17 14:54:12 +0900

    CHANGE, change from yarn to npm and add tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,68 +1,98 @@
 import { db, getData, getDataByQuery } from "../apis/firebase";
-import { collection, getDocs } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
 import { IComment, ILike, IPost, IScrap, ITag, IUser } from "../custom";
 import Feed from "./feed";
 import { GetServerSidePropsContext } from "next";
+import { useEffect, useState } from "react";
+import { useStore } from "../apis/zustand";
 
-interface IIndex {
-  posts: IPost[];
-  users: IUser[];
-}
+export default function Index() {
+  const { curUser } = useStore();
+  const [page, setPage] = useState(1);
+  const [posts, setPosts] = useState<IPost[]>([]);
+  const [users, setUsers] = useState<IUser[]>([]);
 
-export default function Index({ posts, users }: IIndex) {
-  return <Feed posts={posts} users={users} />;
-}
+  useEffect(() => {
+    init();
+  }, []);
+  async function init() {
+    console.log("here");
+    if (curUser.followings.length === 0) {
+      return;
+    }
+    const q = query(
+      collection(db, "posts"),
+      where("uid", "in", [...curUser.followings, curUser.id]),
+      orderBy("createdAt", "desc"),
+      limit(page * 5)
+    );
 
-export async function getServerSideProps() {
-  const postSnap = await getDocs(collection(db, "posts"));
-  const posts: IPost[] = [];
-  const uids: string[] = [];
-  postSnap.forEach((doc) => {
-    posts.push({
-      ...doc.data(),
-      createdAt: doc.data().createdAt.toDate(),
-    } as IPost);
-    uids.push(doc.data().uid);
-  });
-  for await (const post of posts) {
-    const likes = await getDataByQuery<ILike>(
-      "likes",
-      "pid",
-      "==",
-      post.id || ""
-    );
-    const scraps = await getDataByQuery<IScrap>(
-      "scraps",
-      "pid",
-      "==",
-      post.id || ""
-    );
-    const comments = await getDataByQuery<IComment>(
-      "comments",
-      "pid",
-      "==",
-      post.id || ""
-    );
-    if (likes) {
-      post.likes = likes;
-    } else {
-      post.likes = [];
+    // const postSnap = await getDocs(collection(db, "posts"));
+    const postSnap = await getDocs(q);
+    console.log(postSnap);
+    const newPosts: IPost[] = [];
+    const uids: string[] = [];
+    postSnap.forEach((doc) => {
+      newPosts.push({
+        ...doc.data(),
+        createdAt: doc.data().createdAt.toDate(),
+      } as IPost);
+      uids.push(doc.data().uid);
+    });
+    for await (const post of newPosts) {
+      const likes = await getDataByQuery<ILike>(
+        "likes",
+        "pid",
+        "==",
+        post.id || ""
+      );
+      const scraps = await getDataByQuery<IScrap>(
+        "scraps",
+        "pid",
+        "==",
+        post.id || ""
+      );
+      const comments = await getDataByQuery<IComment>(
+        "comments",
+        "pid",
+        "==",
+        post.id || ""
+      );
+      if (likes) {
+        post.likes = likes;
+      } else {
+        post.likes = [];
+      }
+      if (scraps) {
+        post.scraps = scraps;
+      } else {
+        post.scraps = [];
+      }
+      if (comments) {
+        post.comments = comments;
+      } else {
+        post.comments = [];
+      }
     }
-    if (scraps) {
-      post.scraps = scraps;
-    } else {
-      post.scraps = [];
+    const newUsers: IUser[] = [];
+    for await (const uid of uids) {
+      const user = await getData<IUser>("users", uid);
+      newUsers.push(user);
     }
-    if (comments) {
-      post.comments = comments;
-    } else {
-      post.comments = [];
-    }
+    setPosts(newPosts);
+    setUsers(newUsers);
   }
-  const users: IUser[] = [];
-  for await (const uid of uids) {
-    const user = await getData<IUser>("users", uid);
-    users.push(user);
-  }
-  return { props: { posts, users } };
+  return (
+    <>
+      <div className="bg-green-500">zz</div>
+    </>
+  );
+  // return <Feed posts={posts} users={users} />;
 }
