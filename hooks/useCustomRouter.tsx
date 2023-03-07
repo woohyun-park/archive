b57f03af84f71907206b0d34fb1ff6104567dff1
Woohyun Park <woohyunpark.mail@gmail.com>
2023-03-07 22:16:58 +0900

    COMMENT, add comments for useCustomRouter

--- hooks/useCustomRouter.tsx
+++ hooks/useCustomRouter.tsx
@@ -3,6 +3,9 @@ import { wrapPromise } from "../stores/libStores";
 import { useCache } from "../stores/useCache";
 import { useStatus } from "../stores/useStatus";
 
+// 페이지를 이동할 때 이미 cache되어있는 페이지라면 로딩화면을 띄우지 않고,
+// 데이터를 받아와야하는 페이지라면 로딩화면을 띄우도록 도와주는 custom router를 위한 훅
+
 interface CustomNextRouter {
   pushWithLoader: (href: string) => void;
 }
@@ -18,6 +21,8 @@ export default function useCustomRouter() {
     pushWithLoader,
   };
 
+  // 이 함수에서 만약 이동하고자 하는 페이지의 cache가 존재하지 않는다면
+  // 로딩창을 띄운 뒤에 route로 push를 하여 자연스러운 페이지이동이 가능토록 한다.
   async function pushWithLoader(href: string) {
     if (!caches[href]) await wrapPromise(() => setModalLoader(true), 500);
     customRouter.push(href);
