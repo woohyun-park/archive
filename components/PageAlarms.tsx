2ebff9ad677cecadfba469c591ce2f6d6b08ed33
Woohyun Park <woohyunpark.mail@gmail.com>
2023-03-15 15:55:30 +0900

    PROCESS, in fixing logics for alarms page

--- components/PageAlarms.tsx
+++ components/PageAlarms.tsx
@@ -1,28 +1,20 @@
 import { AnimatePresence } from "framer-motion";
 import React, { useEffect, useState } from "react";
-import PullToRefresh from "react-simple-pull-to-refresh";
-import { IFetchQueryAlarms } from "../apis/fbDef";
 import { viewAlarms } from "../apis/fbUpdate";
 import { mergeTailwindClasses } from "../apis/tailwind";
-import { useCachedPage } from "../hooks/useCachedPage";
-import { IAlarm } from "../libs/custom";
 import { useStatus } from "../stores/useStatus";
 import { useUser } from "../stores/useUser";
 import AlarmComment from "./AlarmComment";
 import AlarmFollow from "./AlarmFollow";
 import AlarmLike from "./AlarmLike";
 import WrapMotion from "./wrappers/WrapMotion";
+import WrapPullToRefresh from "./wrappers/WrapPullToRefresh";
 
 export interface IPageAlarmsProps {
-  query: IFetchQueryAlarms;
   className?: string;
 }
 
-export default function PageAlarms({ query, className }: IPageAlarmsProps) {
-  // const { data, canFetchMore, onRefresh, onFetchMore } = useCachedPage(
-  //   "alarms",
-  //   query
-  // );
+export default function PageAlarms({ className }: IPageAlarmsProps) {
   const { curUser } = useUser();
   const { setNotifyAlarms } = useStatus();
   const [alarms, setAlarms] = useState(curUser.alarms);
@@ -40,24 +32,22 @@ export default function PageAlarms({ query, className }: IPageAlarmsProps) {
       curUser.alarms.length !== 0 &&
       curUser.alarms.length !== alarms?.length
     ) {
+      setNotifyAlarms(false);
       setAlarms(curUser.alarms);
       viewAlarms(curUser.alarms);
     }
   }, [curUser.alarms]);
 
   return (
-    <PullToRefresh
-      onRefresh={async () => {
-        // setNotifyAlarms(false);
-        // viewAlarms(data as IAlarm[]);
-      }}
+    <WrapPullToRefresh
+      isPullable={false}
+      onRefresh={async () => {}}
       onFetchMore={async () => {}}
-      // canFetchMore={canFetchMore}
       canFetchMore={false}
       className={mergeTailwindClasses("min-h-[50vh] bg-white", className || "")}
     >
       <AnimatePresence>
-        {alarms.map((alarm, i) => {
+        {alarms?.map((alarm, i) => {
           return (
             <WrapMotion type="float" key={alarm.id}>
               <>
@@ -69,6 +59,6 @@ export default function PageAlarms({ query, className }: IPageAlarmsProps) {
           );
         })}
       </AnimatePresence>
-    </PullToRefresh>
+    </WrapPullToRefresh>
   );
 }
