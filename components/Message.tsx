399151596f3d7587d8cddb62e23948fa14695857
Woohyun Park <woohyunpark.mail@gmail.com>
2023-03-17 13:20:03 +0900

    CHANGE, change ErrorBox component to Message component

---
+++ components/Message.tsx
@@ -0,0 +1,50 @@
+import Image from "next/image";
+import { MouseEventHandler } from "react";
+import useCustomRouter from "../hooks/useCustomRouter";
+import icon_sad from "../imgs/icon_sad.svg";
+import Btn from "./atoms/Btn";
+
+interface IMessage {
+  includeIcon?: boolean;
+  includeBtn?: boolean;
+  message?: string;
+  detailedMessage?: string;
+  label?: string;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+export default function Message({
+  includeIcon = false,
+  includeBtn = false,
+  message,
+  detailedMessage,
+  label,
+  onClick,
+}: IMessage) {
+  //   message={"원하시는 페이지를 찾을 수 없습니다"}
+  //   detailedMessage={`찾으려는 페이지의 주소가 잘못 입력되었거나,
+  // 주소의 변경 혹은 삭제로 인해 사용하실 수 없습니다.
+  // 입력하신 페이지의 주소가 정확한지 다시 한번 확인해 주세요`}
+  //   label="아카이브 홈 가기"
+  //   onClick={() => router.push("/")}
+  const router = useCustomRouter();
+  return (
+    <>
+      <div className="flex items-center w-full min-h-[75vh]">
+        <div className="flex flex-col items-center w-full ">
+          {includeIcon && (
+            <div className="w-24">
+              <Image src={icon_sad} alt="" />
+            </div>
+          )}
+
+          <div className="mb-1 text-xl text-bold">{message}</div>
+          <div className="mb-6 text-sm text-center whitespace-pre-wrap text-gray-2">
+            {detailedMessage}
+          </div>
+          {includeBtn && <Btn label={label || ""} onClick={onClick} />}
+        </div>
+      </div>
+    </>
+  );
+}
--- components/PageAlarms.tsx
+++ components/PageAlarms.tsx
@@ -2,13 +2,14 @@ import { AnimatePresence } from "framer-motion";
 import React, { useState } from "react";
 import { viewAlarm, viewAlarms } from "../apis/fbUpdate";
 import { mergeTailwindClasses } from "../apis/tailwind";
+import useCustomRouter from "../hooks/useCustomRouter";
 import { useLoading } from "../hooks/useLoading";
 import { IAlarm } from "../libs/custom";
 import { useUser } from "../stores/useUser";
 import AlarmComment from "./AlarmComment";
 import AlarmFollow from "./AlarmFollow";
 import AlarmLike from "./AlarmLike";
-import ErrorBox from "./ErrorBox";
+import Message from "./Message";
 import WrapMotion from "./wrappers/WrapMotion";
 import WrapPullToRefresh from "./wrappers/WrapPullToRefresh";
 
@@ -19,54 +20,63 @@ export interface IPageAlarmsProps {
 export default function PageAlarms({ className }: IPageAlarmsProps) {
   const { curUser } = useUser();
   const [alarms, setAlarms] = useState<IAlarm[]>(curUser.alarms || []);
+  const router = useCustomRouter();
 
   useLoading([]);
 
   return (
     <>
-      {alarms.length !== 0 ? (
-        <>
-          <div className="flex justify-end">
-            <div
-              className="mx-4 mb-2 text-xs w-fit text-gray-2f hover:cursor-pointer"
-              onClick={() => viewAlarms(alarms)}
-            >
-              전체 읽음
-            </div>
-          </div>
-          <WrapPullToRefresh
-            onRefresh={async () => setAlarms(curUser.alarms || [])}
-            onFetchMore={async () => {}}
-            canFetchMore={false}
-            className={mergeTailwindClasses(
-              "min-h-[75vh] bg-white",
-              className || ""
-            )}
-          >
-            <AnimatePresence>
-              {alarms?.map((alarm, i) => {
-                return (
-                  <WrapMotion
-                    type="float"
-                    key={alarm.id}
-                    onClick={() => viewAlarm(alarm)}
+      <>
+        <WrapPullToRefresh
+          onRefresh={async () => setAlarms(curUser.alarms || [])}
+          onFetchMore={async () => {}}
+          canFetchMore={false}
+          className={mergeTailwindClasses(
+            "min-h-[75vh] bg-white",
+            className || ""
+          )}
+        >
+          <AnimatePresence>
+            {alarms.length !== 0 ? (
+              <>
+                <div className="flex justify-end">
+                  <div
+                    className="mx-4 mb-2 text-xs w-fit text-gray-2f hover:cursor-pointer"
+                    onClick={() => viewAlarms(alarms)}
                   >
-                    <>
-                      {alarm.type === "like" && <AlarmLike alarm={alarm} />}
-                      {alarm.type === "comment" && (
-                        <AlarmComment alarm={alarm} />
-                      )}
-                      {alarm.type === "follow" && <AlarmFollow alarm={alarm} />}
-                    </>
-                  </WrapMotion>
-                );
-              })}
-            </AnimatePresence>
-          </WrapPullToRefresh>
-        </>
-      ) : (
-        <ErrorBox />
-      )}
+                    전체 읽음
+                  </div>
+                </div>
+                {alarms?.map((alarm, i) => {
+                  return (
+                    <WrapMotion
+                      type="float"
+                      key={alarm.id}
+                      onClick={() => viewAlarm(alarm)}
+                    >
+                      <>
+                        {alarm.type === "like" && <AlarmLike alarm={alarm} />}
+                        {alarm.type === "comment" && (
+                          <AlarmComment alarm={alarm} />
+                        )}
+                        {alarm.type === "follow" && (
+                          <AlarmFollow alarm={alarm} />
+                        )}
+                      </>
+                    </WrapMotion>
+                  );
+                })}
+              </>
+            ) : (
+              <Message
+                includeIcon={false}
+                includeBtn={false}
+                detailedMessage="알림이 없습니다"
+              />
+            )}
+          </AnimatePresence>
+        </WrapPullToRefresh>
+      </>
     </>
   );
 }
