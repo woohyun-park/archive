4c0897c0cbdb22e071276c7bad4b6e5ecfdfa446
Woohyun Park <woohyunpark.mail@gmail.com>
2023-03-23 23:10:10 +0900

    CHORE

--- components/ModalLoader.tsx
+++ components/ModalLoader.tsx
@@ -1,5 +1,4 @@
-import React, { ReactNode, useEffect, useState } from "react";
-import { mergeTailwindClasses } from "../apis/tailwind";
+import React from "react";
 import Modal from "./Modal";
 
 interface IModalLoaderProps {
