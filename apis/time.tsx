7c4c52b40443fa1563e1c6f93f9ff6cd7ce8b78b
Woohyun Park <woohyunpark.mail@gmail.com>
2023-04-15 22:47:32 +0900

    REFACTOR, timeago를 사용하여timeAPI 국제화 지원

--- apis/time.tsx
+++ apis/time.tsx
@@ -1,19 +1,36 @@
 // time을 손쉽게 변환하기 위한 api
 
-import dayjs from "dayjs";
 import { FieldValue } from "firebase/firestore";
+import { format, register } from "timeago.js";
+
+type IRegister = [string, string][];
+
+const localeFunc = (
+  number: number,
+  index: number,
+  totalSec?: number
+): [string, string] => {
+  const ko: IRegister = [
+    ["지금", "지금"],
+    ["%s초 전", "%s초 후"],
+    ["1분 전", "1분 후"],
+    ["%s분 전", "%s분 후"],
+    ["1시간 전", "1시간 후"],
+    ["%s시간 전", "%s시간 후"],
+    ["1일 전", "1일 후"],
+    ["%s일 전", "%s일 후"],
+    ["1주 전", "1주 후"],
+    ["%s주 전", "%s주 후"],
+    ["1달 전", "1달 후"],
+    ["%s달 전", "%s달 후"],
+    ["1년 전", "1년 후"],
+    ["%s년 전", "%s년 후"],
+  ];
+  return ko[index];
+};
+
+register("ko-KR", localeFunc);
 
 export function displayCreatedAt(createdAt: Date | FieldValue) {
-  const curDate = dayjs(new Date());
-  const postDate = dayjs(createdAt as Date);
-  if (curDate.diff(postDate) < 0) return "0초 전";
-  if (curDate.diff(postDate) < 60000)
-    return `${curDate.diff(postDate, "s")}초 전`;
-  if (curDate.diff(postDate) < 3600000)
-    return `${curDate.diff(postDate, "m")}분 전`;
-  if (curDate.diff(postDate) < 86400000)
-    return `${curDate.diff(postDate, "h")}시간 전`;
-  if (curDate.diff(postDate) < 86400000 * 3)
-    return `${curDate.diff(postDate, "d")}일 전`;
-  return postDate.format("MM월 DD, YYYY");
+  return format(createdAt as Date, navigator.language);
 }
